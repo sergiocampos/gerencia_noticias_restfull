125f6f687faf823c1b1871de88ba6468dc5a2fdf
sergiocampos <sergio.campos@dce.ufpb.br>
2017-04-04 00:20:56 -0300

    criação da entidade noticia, atributos e metodos.

---
+++ GerenciaNoticias/src/java/br/pd/Noticia.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.pd;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author sergio
+ */
+@Entity
+public class Noticia implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String autor;
+    private String titulo;
+    private String dataDaNoticia;
+    private String conteudo;
+
+    public Noticia(){
+        
+    }
+    
+    public Noticia(Long id, String autor, String titulo, String dataDaNoticia,
+            String conteudo){
+        this.id = id;
+        this.autor = autor;
+        this.titulo = titulo;
+        this.dataDaNoticia = dataDaNoticia;
+        this.conteudo = conteudo;
+        
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDataDaNoticia() {
+        return dataDaNoticia;
+    }
+
+    public void setDataDaNoticia(String dataDaNoticia) {
+        this.dataDaNoticia = dataDaNoticia;
+    }
+
+    
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+    
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Noticia)) {
+            return false;
+        }
+        Noticia other = (Noticia) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Noticia[ id=" + id + " ]";
+    }
+    
+}
